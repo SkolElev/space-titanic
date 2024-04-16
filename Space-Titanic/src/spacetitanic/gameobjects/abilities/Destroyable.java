c533951033f60b5f00f547e83b158f1fad7c66bd
SkolElev <156174877+SkolElev@users.noreply.github.com>
2024-04-16 10:57:10 +0200

    2024-04-16 banana split

---
+++ Space-Titanic/src/spacetitanic/gameobjects/abilities/Destroyable.java
@@ -0,0 +1,14 @@
+package spacetitanic.gameobjects.abilities;
+
+public interface Destroyable {
+
+
+    public void destroyed();
+
+    public void takeDamage(int damage);
+
+    /*public boolean isTargeted();*/
+
+    /*public void setTargeted();*/
+
+}
