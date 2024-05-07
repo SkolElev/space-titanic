aeb2671eea9055b7ac51d6b6c4ad95f83a698bfb
SkolElev <156174877+SkolElev@users.noreply.github.com>
2024-05-07 11:28:32 +0200

    Add files via upload

---
+++ src/spacetitanic/gameobjects/abilities/Delay.java
@@ -0,0 +1,12 @@
+package spacetitanic.gameobjects.abilities;
+
+public interface Delay {
+
+    public boolean isDelayed();
+
+    public boolean checkDelayed();
+
+
+
+
+}
---
+++ src/spacetitanic/gameobjects/equipments/Equipment.java
@@ -0,0 +1,88 @@
+package spacetitanic.gameobjects.equipments;
+
+import spacetitanic.GamePanel;
+import spacetitanic.gameobjects.GameObject;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public abstract class Equipment {
+    protected GamePanel gamePanel;
+    protected Hardpoint hardpoint;
+    protected EquipmentType equipmentType;
+    protected HardpointType hardpointType;
+    protected BufferedImage[] equipmentImages;
+    protected String equipmentName;
+    protected int imageNumber = 0;
+
+    protected boolean reloading = false, manualReloadAllowed = true;
+    protected int ammo, maxAmmo, damage, range, fireDelay;
+    protected double reloadTime, projectileSpeed;
+    protected BufferedImage projectileImage, ammoImage;
+
+
+    public Equipment(GamePanel gamePanel, Hardpoint hardpoint) {
+        this.gamePanel = gamePanel;
+        this.hardpoint = hardpoint;
+    }
+
+    public abstract void update();
+
+    public abstract void render(Graphics2D g2);
+
+    public abstract void activate();
+
+    public abstract void activate(GameObject target);
+
+    public abstract void deactivate();
+
+    public void reload() {
+        if (manualReloadAllowed) {
+            reloading = true;
+        }
+    }
+
+    public EquipmentType getEquipmentType() {
+        return equipmentType;
+    }
+
+    public HardpointType getHardpointType() {
+        return hardpointType;
+    }
+
+    public String getEquipmentName() {
+        return equipmentName;
+    }
+
+    public boolean isReloading() {
+        return reloading;
+    }
+
+    public int getAmmo() {
+        return ammo;
+    }
+
+    public int getMaxAmmo() {
+        return maxAmmo;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public int getFireDelay() {
+        return fireDelay;
+    }
+
+    public double getReloadTime() {
+        return reloadTime;
+    }
+
+    public BufferedImage getAmmoImage() {
+        return ammoImage;
+    }
+}
