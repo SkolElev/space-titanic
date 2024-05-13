449b0238404d5df943cb8af6e8e4580727a9759c
SkolElev <156174877+SkolElev@users.noreply.github.com>
2024-05-13 10:30:04 +0200

    Delete src/spacetitanic directory

--- src/spacetitanic/gameobjects/abilities/Profit.java
+++
@@ -1,9 +0,0 @@
-package spacetitanic.gameobjects.abilities;
-
-public interface Profit {
-
-    /* Add an int value */
-
-    /* Generate Resource of the given value */
-    public void generateResource(int value);
-}
